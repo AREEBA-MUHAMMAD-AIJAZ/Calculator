628039c0d9ccdf8ece6a4c8c7489e6591ea8d9e5
AREEBA-MUHAMMAD-AIJAZ <>
2023-08-03 03:09:48 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,46 @@
+// import React from 'react'
+import inquirer from 'inquirer';
+
+const answer : {
+    numberOne:number,
+    numbertwo:number,
+    operator:string
+
+}= await inquirer.prompt([
+    {
+        type:"number",
+        name:"numberOne",
+        message:"kindly enter ypu 1st no:"
+    },
+    {
+        type:"number",
+        name:"numbertwo",
+        message:"kindly enter ypu sec no:"
+    },
+    {
+        type:"list",
+        name:"operator",
+        choices:["*","+","-","/"],
+        message:"Select Operator"
+    }
+]);
+
+// console.log(answer);
+
+const {numberOne,numbertwo,operator}  = answer;
+if(numberOne&&numbertwo&&operator){
+    let result:number = 0;
+    if(operator === "*"){
+        result = numbertwo * numberOne
+    } else if (operator === "+"){
+        result = numbertwo + numberOne
+    }else if (operator === "-"){
+        result = numbertwo - numberOne
+    }if (operator === "/"){
+        result = numbertwo / numberOne
+    }
+
+    console.log("your result is :", result);
+    
+} else {"please enter a valid number"}
+
